2148e2ab93ae1f4af81afc7f0ecdb4dbf23dfe40
SimoneLaRocca <profilostudio11@gmail.com>
2022-06-07 23:49:11 +0200

    home

---
+++ app/src/main/java/it/unisa/walletmanagement/Control/GestioneConti/ContoAdapter.java
@@ -0,0 +1,39 @@
+package it.unisa.walletmanagement.Control.GestioneConti;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+import it.unisa.walletmanagement.Model.Entity.Conto;
+import it.unisa.walletmanagement.R;
+
+public class ContoAdapter extends ArrayAdapter<Conto> {
+    public ContoAdapter(@NonNull Context context, int resource, @NonNull List<Conto> objects) {
+        super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if(convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_view_conto_element, parent, false);
+        }
+        Conto conto = getItem(position);
+
+        TextView tvNome = convertView.findViewById(R.id.tv_nome_conto);
+        TextView tvSaldo = convertView.findViewById(R.id.tv_saldo_conto);
+
+        tvNome.setText(conto.getNome());
+        tvSaldo.setText("€ "+Float.toString(conto.getSaldo()));
+
+        return convertView;
+    }
+}
--- app/src/main/java/it/unisa/walletmanagement/Control/GestioneConti/HomeActivity.java
+++ app/src/main/java/it/unisa/walletmanagement/Control/GestioneConti/HomeActivity.java
@@ -3,15 +3,39 @@ package it.unisa.walletmanagement.Control.GestioneConti;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+
+import it.unisa.walletmanagement.Model.Entity.Conto;
 import it.unisa.walletmanagement.R;
 
 public class HomeActivity extends AppCompatActivity {
+
+    ContoAdapter contoAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        ListView listViewConto = findViewById(R.id.list_view_conti);
+        contoAdapter = new ContoAdapter(this, R.layout.list_view_conto_element, new ArrayList<Conto>());
+        listViewConto.setAdapter(contoAdapter);
+
+        for (int i = 0; i<100; i++){
+            Conto esempio_conto = new Conto("Lavoro", 2000f, null, "");
+            contoAdapter.add(esempio_conto);
+        }
+
+        listViewConto.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
     }
 
 
